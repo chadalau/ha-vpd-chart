5494a30fbeada6db23a4e1092472b161578d5772
chadalau <chadalau@users.noreply.github.com>
2026-07-19 09:26:42 -0300

    Revert date labels on history axis

--- dist/bar.js
+++ dist/bar.js
@@ -15,7 +15,7 @@ export const bar = {
                     </div>
                 </ha-card>
         `;
-        const cssUrl = new URL('bar.css?v=3.3.3', import.meta.url).href;
+        const cssUrl = new URL('bar.css?v=3.3.4', import.meta.url).href;
         this.innerHTML = this.htmlTemplate.replace('##url##', cssUrl);
         this.content = this.querySelector("div.card-content");
 
